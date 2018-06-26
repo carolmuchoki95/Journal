74e5ec5ce5e6f90b143207606eff311b940f0e97
carolmuchoki95 <carolmuchoki95@gmail.com>
2018-06-27 00:02:45 +0300

    Sign up using firebase

---
+++ app/src/main/java/com/muchoki/carol/journal/JournalEntries.java
@@ -0,0 +1,13 @@
+package com.muchoki.carol.journal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JournalEntries extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal_entries);
+    }
+}
